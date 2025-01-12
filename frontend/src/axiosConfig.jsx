3c9267d502a93ec93ec8ce24519171ba8f0a9a08
vdot <vittorio.worang@gmail.com>
2025-01-13 02:06:42 +1100

    edited baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import config from '../backend.config.json'; 
+import axios from "axios";
+// import config from '../backend.config.json';
 
-const baseURL = `http://localhost:${config.BACKEND_PORT}`;
+const baseURL = `http:presto-deploy2-n27fcjaz5-vittos-projects-4de1ee50.vercel.app`;
 
 const instance = axios.create({
   baseURL: baseURL,
 });
 
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
